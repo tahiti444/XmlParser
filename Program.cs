45ac505144e39d7eed876a20de2fc0c68360029e
David Galve Masip <davidgalvemasip@gmail.com>
2022-02-18 13:10:13 +0100

    parsing to Json

--- Program.cs
+++ Program.cs
@@ -1,14 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Linq;
 
 
 namespace vdv
 {
     public class Program
     {
+        public static string PROJECTPATH = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName; // Visual Studio (bin)
+        //public string PROJECTPATH = Path.Combine(
+        //    Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName,
+        //    "UserRights"
+        //); // UnitTest
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var path = Path.Combine(PROJECTPATH, "data");
+            //string path = @"C:\Projekte\793_Stadtverkehr_Luebeck\Stoerungsmelder\PS - Scripts\cache\of.xml";
+            string fileText = File.ReadAllText(Path.Combine(path, "of.xml"));
+            XmlParser parser = new XmlParser(fileText);
+            parser.ConvertToJson();
+            Console.ReadLine();
         }
 
     }
--- XmlParser.cs
+++ XmlParser.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Linq;
+using System.Xml;
+using Newtonsoft.Json;
+
 namespace vdv
 {
     public class XmlParser
     {
-        
+        public string XmlText { get; set; }
+        XmlDocument XmlDocument { get; set; }
+
+        public XmlParser (string _xmlText)
+        {
+            XmlText = _xmlText;
+            XmlDocument = new XmlDocument ();
+        }
+
+        public void ConvertToJson(int indent=2)
+        {
+            XmlDocument.LoadXml(XmlText);
+            string jsonText = JsonConvert.SerializeXmlNode(XmlDocument, Newtonsoft.Json.Formatting.Indented, true);
+            Console.WriteLine(jsonText);
+        }
     }
 }
